99e66a819930a00a2c23ac376572708f7de69b93
Vadym Zelenko <115219323+01cheese@users.noreply.github.com>
2024-09-20 21:29:16 +0200

    share / not design

--- share.js
+++ share.js
@@ -1,3 +1,63 @@
+let currentPage = 1;
+
+
+// Инициализация темы при загрузке
+document.addEventListener('DOMContentLoaded', () => {
+    // Восстановление сохраненной темы
+    const savedTheme = localStorage.getItem('selectedTheme');
+    const themeOptions = document.querySelectorAll('.theme-option');
+
+    if (savedTheme) {
+        // Применяем сохраненную тему
+        document.body.className = `${savedTheme}-theme`;
+
+        // Активируем кнопку выбранной темы
+        themeOptions.forEach(option => {
+            const theme = option.getAttribute('data-theme');
+            if (theme === savedTheme) {
+                option.classList.add('active'); // Добавляем класс "active" к сохраненной теме
+            } else {
+                option.classList.remove('active');
+            }
+        });
+    } else {
+        // Если тема не сохранена, применяем тему по умолчанию
+        const defaultTheme = 'grey'; // Укажите тему по умолчанию
+        document.body.className = `${defaultTheme}-theme`;
+        const defaultOption = document.querySelector(`.theme-option[data-theme="${defaultTheme}"]`);
+        if (defaultOption) {
+            defaultOption.classList.add('active');
+        }
+    }
+
+    // Загрузка постов на текущей странице
+    loadPosts(currentPage);
+
+    // Инициализация переключателя тем
+    initThemeSwitcher();
+});
+
+// Логика переключения тем
+function initThemeSwitcher() {
+    const themeSwitcher = document.getElementById('theme-switcher');
+    const themeOptions = document.querySelectorAll('.theme-option');
+
+    themeOptions.forEach(option => {
+        option.addEventListener('click', function () {
+            const selectedTheme = this.getAttribute('data-theme');
+            document.body.className = `${selectedTheme}-theme`;
+
+            // Сохранение выбранной темы в localStorage
+            localStorage.setItem('selectedTheme', selectedTheme);
+
+            // Обновляем активное состояние кнопок
+            themeOptions.forEach(opt => opt.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+}
+
+
 // Function to get URL parameters
 function getUrlParams() {
     const params = new URLSearchParams(window.location.search);
